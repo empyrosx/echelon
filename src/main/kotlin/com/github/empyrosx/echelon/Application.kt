c036c416920269499c039f5986316239126344e6
Zimichev Dmitrii <empyros@yandex-team.ru>
2023-12-10 02:34:55 +0300

    Initial commit

---
+++ src/main/kotlin/com/github/empyrosx/echelon/Application.kt
@@ -0,0 +1,7 @@
+package com.github.empyrosx.echelon
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/kotlin/com/github/empyrosx/echelon/EchelonAppTest.kt
@@ -0,0 +1,19 @@
+package com.github.empyrosx.echelon
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class EchelonAppTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
